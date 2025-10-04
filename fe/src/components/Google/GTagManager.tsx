d9100481e30e0fe9d1f0776f9ba27f7c86fa5df8
TAI\tailt <letuantai241@gmail.com>
2025-10-05 03:37:12 +1030

    fix: remove comment html

--- fe/src/components/Google/GTagManager.tsx
+++ fe/src/components/Google/GTagManager.tsx
@@ -14,23 +14,23 @@ const GTagManager: React.FC<GTMProps> = ({
       {position == "head" && (
         <script
           dangerouslySetInnerHTML={{
-            __html: `<!-- Google Tag Manager -->
+            __html: `
 (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
 new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
 j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
 'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
 })(window,document,'script','dataLayer','${GTMId}');
-<!-- End Google Tag Manager -->`,
+`,
           }}
         ></script>
       )}
       {position == "body" && (
         <noscript
           dangerouslySetInnerHTML={{
-            __html: `<!-- Google Tag Manager (noscript) -->
+            __html: `
 <iframe src="https://www.googletagmanager.com/ns.html?id=${GTMId}"
 height="0" width="0" style="display:none;visibility:hidden"></iframe>
-<!-- End Google Tag Manager (noscript) -->`,
+`,
           }}
         ></noscript>
       )}
